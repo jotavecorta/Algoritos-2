5c9d127bbd52e4e9028dec76eae9ef10a8ddf7dc
Jotavecorta <julianvilla.90@gmail.com>
2023-05-06 17:16:01 -0300

    Modifiqué gitignore

---
+++ src/labo03/CMakeFiles/ej1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jotavecorta/proyects/Algoritmos-2/src/labo03/AyED2-Labo03-Memoria_dinamica/src/ConexionJugador.cpp" "CMakeFiles/ej1.dir/src/ConexionJugador.cpp.o" "gcc" "CMakeFiles/ej1.dir/src/ConexionJugador.cpp.o.d"
+  "/home/jotavecorta/proyects/Algoritmos-2/src/labo03/AyED2-Labo03-Memoria_dinamica/src/Internet.cpp" "CMakeFiles/ej1.dir/src/Internet.cpp.o" "gcc" "CMakeFiles/ej1.dir/src/Internet.cpp.o.d"
+  "/home/jotavecorta/proyects/Algoritmos-2/src/labo03/AyED2-Labo03-Memoria_dinamica/src/SistemaDeMensajes.cpp" "CMakeFiles/ej1.dir/src/SistemaDeMensajes.cpp.o" "gcc" "CMakeFiles/ej1.dir/src/SistemaDeMensajes.cpp.o.d"
+  "/home/jotavecorta/proyects/Algoritmos-2/src/labo03/AyED2-Labo03-Memoria_dinamica/tests/sdm_tests.cpp" "CMakeFiles/ej1.dir/tests/sdm_tests.cpp.o" "gcc" "CMakeFiles/ej1.dir/tests/sdm_tests.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jotavecorta/proyects/Algoritmos-2/src/labo03/AyED2-Labo03-Memoria_dinamica/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/jotavecorta/proyects/Algoritmos-2/src/labo03/AyED2-Labo03-Memoria_dinamica/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
